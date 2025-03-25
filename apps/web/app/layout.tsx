6433bdb56f8371ba2f4ba28e6f7ad1c2e6fccbf7
yougncheon <kimyoungcheon97@gmail.com>
2025-03-26 00:19:51 +0900

    feat: 글꼴 Pretendard 추가

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,18 +2,15 @@
 
 import './globals.css';
 import '@repo/ui/styles.css';
-import { Inter } from 'next/font/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const inter = Inter({ subsets: ['latin'] });
 
 const queryClient = new QueryClient();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body>
         <QueryClientProvider client={queryClient}>
           {children}
           <ReactQueryDevtools initialIsOpen={false} />
