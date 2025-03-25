1a706075d735c885bc2457d4bb5fbbdc0044e3d7
yougncheon <kimyoungcheon97@gmail.com>
2025-03-25 12:16:50 +0900

    feat: hooks 패키지 생성 및 디바운스, 쓰로틀 훅 생성

---
+++ packages/hooks/src/use-throttle.ts
@@ -0,0 +1,30 @@
+import { useCallback, useRef } from 'react';
+
+const useThrottle = <T extends (...args: any[]) => any>(callback: T, delay: number) => {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const lastExecutedRef = useRef<number>(0);
+
+  return useCallback(
+    (...args: Parameters<T>) => {
+      const now = Date.now();
+
+      if (now - lastExecutedRef.current >= delay) {
+        callback(...args);
+        lastExecutedRef.current = now;
+      } else {
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = setTimeout(() => {
+          callback(...args);
+          lastExecutedRef.current = Date.now();
+          timeoutRef.current = null;
+        }, delay);
+      }
+    },
+    [callback, delay],
+  );
+};
+
+export default useThrottle;
---
+++ packages/types/src/index.d.ts
@@ -0,0 +1,9 @@
+export interface Data {
+    id: string;
+    author: string;
+    width: number;
+    height: number;
+    url: string;
+    download_url: string;
+}
+//# sourceMappingURL=index.d.ts.map
