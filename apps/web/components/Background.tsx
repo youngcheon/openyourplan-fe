33d4b69a6a92e5d239186515ede22675cff520dd
yougncheon <kimyoungcheon97@gmail.com>
2025-03-27 00:26:41 +0900

    style: background 마스크 레이어 스타일 수정

--- apps/web/components/Background.tsx
+++ apps/web/components/Background.tsx
@@ -7,10 +7,10 @@ interface BackgroundProps {
 const Background = ({ imageUrl }: BackgroundProps) => {
   return (
     <>
-      <Image src={imageUrl} alt="image" fill className="absolute -z-10 object-cover blur-2xl" />
-      <Image src="/pattern.png" alt="background" fill className="absolute -z-10 object-cover blur-lg" />
-      <div className="absolute -z-10 h-full w-full bg-black/30" />
-      <div className="absolute -z-10 h-full w-full bg-gradient-to-b from-transparent to-[#D9D9D9]" />
+      <Image src={imageUrl} alt="image" fill className="fixed -z-10 object-cover blur-2xl" />
+      <Image src="/pattern.png" alt="background" fill className="fixed -z-10 object-cover blur-lg" />
+      <div className="fixed top-0 -z-10 h-full w-full bg-black/30" />
+      <div className="fixed top-0 -z-10 h-full w-full bg-gradient-to-b from-[#D9D9D9]/30 to-[#D9D9D9]" />
     </>
   );
 };
